5da9c302213153a4d91f6e9a6ed135d249c6951c
Andry Saprigin <coffeei.2002@gamil.com>
2022-12-30 01:23:26 +0300

    commit

--- src/app/new-module/components/products/products.component.ts
+++ src/app/new-module/components/products/products.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  DoCheck,
+  ElementRef,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { IProduct } from 'src/app/interfaces/product';
 import { ApiProductService } from 'src/app/services/api-product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css'],
+  styleUrls: ['./products.component.css', './loader.css', './preloader.css'],
 })
 export class ProductsComponent implements OnInit {
+  @ViewChild('preloader') preloader: ElementRef<HTMLDivElement>;
+  @ViewChild('loader') loader: ElementRef<HTMLDivElement>;
   public products: IProduct[];
 
   constructor(private productService: ApiProductService) {}
@@ -15,5 +26,15 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
     this.productService.loadAllProducts();
     this.products = this.productService.getProducts();
+
+    var preloader = document.body.getElementsByClassName('preloader')[0];
+    var content = document.body.getElementsByClassName('content')[0];
+
+    setTimeout(() => {
+      if (!preloader.classList.contains('done')) {
+        content.classList.add('done-content');
+        preloader.classList.add('done-preloader');
+      }
+    }, 2000);
   }
 }
